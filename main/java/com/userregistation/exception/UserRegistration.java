ea882ceb71195c5cac45ee2e75458bc8cadb725f
renu100 <renu.chandraker08@gmail.com>
2021-10-15 22:25:18 +0530

    Added: Program for the last name is in format or not

--- main/java/com/userregistation/exception/UserRegistration.java
+++ main/java/com/userregistation/exception/UserRegistration.java
@@ -5,15 +5,21 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	// create method for first name
+	// create method to check first name
 	public boolean firstName(String firstName) {
-
 		String regex = "^[A-z]{1}[a-z]*";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher match = pattern.matcher(firstName);
-
 		return match.matches();
 
+	}
+
+	// create method to check last name
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{4}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
 	}
 
 }
--- test/java/com/userregistation/exception/UserRegistrationTest.java
+++ test/java/com/userregistation/exception/UserRegistrationTest.java
@@ -7,18 +7,33 @@ public class UserRegistrationTest {
 
 	UserRegistration userRegistration = new UserRegistration();
 
-	// Testcase for valid name
+	// Test case for valid firstname
 	@Test
 	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
 		boolean result = userRegistration.firstName("Renu");
 		Assert.assertEquals(true, result);
 	}
 
-	// Testcase for invalid name
+	// Test case for invalid firstname
 	@Test
 	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
 		boolean result = userRegistration.firstName("renu");
 		Assert.assertEquals(false, result);
+	}
+
+	// Test case for valid lastname
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Chandraker");
+		Assert.assertEquals(true, result);
+	}
+
+	// Test case for invalid lastname
+	@Test
+	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("chandraker");
+		Assert.assertEquals(false, result);
+
 	}
 
 }
