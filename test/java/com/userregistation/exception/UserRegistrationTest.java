bc660b8d8323106d647e468464515f934c270c3d
renu100 <renu.chandraker08@gmail.com>
2021-10-18 15:15:48 +0530

    Added: User need to enter a valid email

--- test/java/com/userregistation/exception/UserRegistrationTest.java
+++ test/java/com/userregistation/exception/UserRegistrationTest.java
@@ -7,33 +7,47 @@ public class UserRegistrationTest {
 
 	UserRegistration userRegistration = new UserRegistration();
 
-	// Test case for valid firstname
+	// Test case for valid first name
 	@Test
 	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
 		boolean result = userRegistration.firstName("Renu");
 		Assert.assertEquals(true, result);
 	}
 
-	// Test case for invalid firstname
+	// Test case for invalid first name
 	@Test
 	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
 		boolean result = userRegistration.firstName("renu");
 		Assert.assertEquals(false, result);
 	}
 
-	// Test case for valid lastname
+	// Test case for valid last name
 	@Test
 	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
 		boolean result = userRegistration.lastName("Chandraker");
 		Assert.assertEquals(true, result);
 	}
 
-	// Test case for invalid lastname
+	// Test case for invalid last name
 	@Test
 	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
 		boolean result = userRegistration.lastName("chandraker");
 		Assert.assertEquals(false, result);
 
+	}
+
+	// Test case for valid Email address
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("abc@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+
+	// Test case for invalid Email address
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*gmail.com");
+		Assert.assertEquals(false, result);
 	}
 
 }
